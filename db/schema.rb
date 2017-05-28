c38473584a6a9d1c294dadc47df7ab6c715512a7
Nail Zagidulin <karimnail96@gmail.com>
2017-05-28 14:02:33 +0300

    Add Book model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170524102514) do
+ActiveRecord::Schema.define(version: 20170528105224) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "answers", force: :cascade do |t|
     t.text     "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
---
+++ spec/factories/books.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :book do
+    title "Essential Grammar in Use"
+  end
+end
